cf24ef876a58890a56f5958d6a9c05d7447b0e31
SweOlle <80258629+SweOlle@users.noreply.github.com>
2024-05-05 13:00:49 +0200

    Add WasRun subclass of TestCase with code skeleton.

--- TypeUnitC++/TypeUnit.cpp
+++ TypeUnitC++/TypeUnit.cpp
@@ -3,29 +3,43 @@ using namespace std;
 
 class TestCase
 {
-public:
+private:
     string name;
 
-    TestCase(string name1) // Constructor / init
-    {
-        name = name1;
-    }
-
-    void run() // Should run the Test
+protected:
+    virtual void setUp()
     {
     }
 
-    void setUp()
+    virtual void tearDown()
     {
     }
 
-    void tearDown()
+public:
+    TestCase(string name1) : name(name1) {}
+
+    virtual void run() // Should run the Test
     {
     }
 };
 
-/*
-Public
-Private
-Protected
-*/
+class WasRun : public TestCase
+{
+private:
+    bool wasRun;
+    bool wasSetUp;
+    string log;
+
+public:
+    WasRun(string name1) : TestCase(name1) {}
+
+    void run() // Should run the Test.
+    {
+        // Use override? or is it different implementations? We have to only consider the UML model and not Becks example here.
+        // We get to decide how to implement the functions since our UML Model does not cover the implementation logic
+    }
+
+    void testMethod()
+    {
+    }
+};
